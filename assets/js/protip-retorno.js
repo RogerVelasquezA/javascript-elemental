9b61927a4ac2b01e92983d77e3d5efbf0f8807dd
rvelasquez <rogervelasquez666@gmail.com>
2023-09-26 19:54:34 -0500

    protip

--- assets/js/protip-retorno.js
+++ assets/js/protip-retorno.js
@@ -14,3 +14,22 @@ imprimirArgumentos(10, true, 'Hola mundo');
 const[edad, nota, nombre, vivo] = imprimirArgumentos2(23,34,'Roger', false)
 
 console.log({edad,vivo})
+
+const tony = {
+    nombre: 'Tony Stark',
+    codeName: 'Aeroman',
+    vivo: false,
+    edad: 35,
+    trajes: ['Mark I', 'Mark II'],
+};
+
+const imprimirPersonajes = ({nombre, codeName, vivo, edad,trajes, area="informatica"}) =>{
+ 
+    console.log({nombre})
+    console.log({codeName})
+    console.log({vivo})
+    console.log({edad})
+    console.log({trajes})
+    console.log({area})
+}
+ imprimirPersonajes({tony})

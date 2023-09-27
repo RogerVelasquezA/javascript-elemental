d685c84a4969c437f0562d6197f1a8bdd5fad555
rvelasquez <rogervelasquez66@gmail.com>
2023-09-27 09:45:37 -0500

    commit mac

--- assets/js/protip-retorno.js
+++ assets/js/protip-retorno.js
@@ -10,7 +10,6 @@ console.log(imprimirArgumentos2(23,34,'Roger', false))
 console.log(crearPersona('Roger','Velasquez'));
 imprimirArgumentos(10, true, 'Hola mundo');
 
-//Segunda prueba
 const[edad, nota, nombre, vivo] = imprimirArgumentos2(23,34,'Roger', false)
 
 console.log({edad,vivo})

5c8cedc541fd2da89896bc7bc24b4981b8923a73
rvelasquez <rogervelasquez66@gmail.com>
2023-10-05 16:12:54 -0500

    switch

---
+++ assets/js/bases/switch.js
@@ -0,0 +1,16 @@
+const dia = 1
+
+switch(dia){//por defecto hace la comparacion con el ===
+    case 0:
+        console.log('Domingo')
+        break;
+    case 1:
+        console.log('Lunes')
+        break;
+    case 2:
+        console.log('Martes')
+        break;
+    default:
+        console.log('Otro dia')
+        break
+}

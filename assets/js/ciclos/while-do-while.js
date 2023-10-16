a558cf193da7ded9495a3c68329d0280523b0993
rvelasquez <rogervelasquez66@gmail.com>
2023-10-16 00:17:34 -0500

    while-do-whilem final

--- assets/js/ciclos/while-do-while.js
+++ assets/js/ciclos/while-do-while.js
@@ -1,5 +1,5 @@
 const auto = ['Mazda','Honda','Hyundai', 'Toyota'];
-
+//               0       1        2          3
 // let indice = 0;
 
 // while(indice < auto.length){
@@ -14,8 +14,19 @@ let indice2 = 0;
 while(auto[indice2]){
     if(indice2 === 1){
         //break;
+        indice2++;
         continue;
+
     }
     console.log(auto[indice2])
     indice2 ++;
 }
+
+console.warn("do while")
+
+let indice3=0
+ do{
+    console.log(auto[indice3]);
+    indice3++;
+
+ }while(auto[indice3])
